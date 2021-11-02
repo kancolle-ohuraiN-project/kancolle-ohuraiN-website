84327f53c5fc59c3f76da70b6b86ecb5b7d4c746
misaka10843 <misaka10843@outlook.jp>
2021-11-02 14:54:06 +0800

    终于修了按钮的margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,25 +8,21 @@ import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  var style={'flex-wrap': 'wrap'}
+  var buttonstyle={margin:'20px 36px'}
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg "
-            to="/docs/index">
+        <div className={styles.buttons} style={style}>
+          <Link className="button button--secondary button--lg" style={buttonstyle} to="/docs/index">
             浏览开发文档📑
           </Link>
-          <Link
-            className="githubbutton button button--secondary button--lg"
-            to="https://github.com/offline-colle/">
+          <Link className="button button--secondary button--lg" to="https://github.com/offline-colle/">
             查看GitHub项目🖥️
           </Link>
-          <Link
-            className="button button--secondary button--lg" 
-            to="https://discord.gg/YKqytuN7PQ">
+          <Link className="button button--secondary button--lg" style={buttonstyle} to="https://discord.gg/YKqytuN7PQ">
             加入discord频道💬
           </Link>
         </div>
