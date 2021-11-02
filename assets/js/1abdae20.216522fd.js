6dd7c31dbfcbfaa7543e975da88ddbb14dfe8507
misaka10843 <misaka10843@outlook.jp>
2021-11-02 18:40:09 +0800

    Deploy website - based on b96ae9122b81c4b513d4a24be0b1b3a50f2908a2

---
+++ assets/js/1abdae20.216522fd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkancolle=self.webpackChunkkancolle||[]).push([[923],{9060:function(e){e.exports=JSON.parse('{"permalink":"/offline-colle-website/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
